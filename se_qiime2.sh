5c15fe72f675eddc32d3f429c8967dd925d245a3
Andrew Ndhlovu <drewxdvst@outlook.com>
2018-11-30 06:02:10 +0200

    add more steps in pipeline

--- se_qiime2.sh
+++ se_qiime2.sh
@@ -31,6 +31,8 @@ source activate qiime2-2018.8
 
 
 
+## Dada2 
+
 # qiime dada2 denoise-single \
 #       --i-demultiplexed-seqs demux.qza \
 #       --p-trim-left 0 \
@@ -42,6 +44,178 @@ source activate qiime2-2018.8
 
 
 
+# qiime metadata tabulate \
+#       --m-input-file stats-dada2.qza \
+#       --o-visualization stats-dada2.qzv
+
+
+
+## Deblur
+qiime quality-filter q-score \
+      --i-demux demux.qza \
+      --o-filtered-sequences demux-filtered.qza \
+       --o-filter-stats demux-filter-stats.qza
+
+
+qiime deblur denoise-16S \
+      --i-demultiplexed-seqs demux-filtered.qza \
+      --p-trim-length 120 \
+      --o-representative-sequences rep-seqs-deblur.qza \
+      --o-table table-deblur.qza \
+      --p-sample-stats \
+        --o-stats deblur-stats.qza
+
+
 qiime metadata tabulate \
-      --m-input-file stats-dada2.qza \
-      --o-visualization stats-dada2.qzv
+      --m-input-file demux-filter-stats.qza \
+      --o-visualization demux-filter-stats.qzv
+qiime deblur visualize-stats \
+      --i-deblur-stats deblur-stats.qza \
+        --o-visualization deblur-stats.qzv
+
+
+mv rep-seqs-dada2.qza rep-seqs.qza
+mv table-dada2.qza table.qza
+
+
+# mv rep-seqs-deblur.qza rep-seqs.qza
+# mv table-deblur.qza table.qza
+
+##FeatureTable and FeatureData summaries
+
+qiime feature-table summarize \
+      --i-table table.qza \
+      --o-visualization table.qzv \
+      --m-sample-metadata-file sample-metadata.tsv
+qiime feature-table tabulate-seqs \
+      --i-data rep-seqs.qza \
+        --o-visualization rep-seqs.qzv
+
+
+
+qiime phylogeny align-to-tree-mafft-fasttree \
+      --i-sequences rep-seqs.qza \
+      --o-alignment aligned-rep-seqs.qza \
+      --o-masked-alignment masked-aligned-rep-seqs.qza \
+      --o-tree unrooted-tree.qza \
+      --o-rooted-tree rooted-tree.qza
+
+
+
+qiime diversity core-metrics-phylogenetic \
+      --i-phylogeny rooted-tree.qza \
+      --i-table table.qza \
+      --p-sampling-depth 1109 \
+      --m-metadata-file sample-metadata.tsv \
+      --output-dir core-metrics-results
+
+
+
+
+qiime diversity alpha-group-significance \
+      --i-alpha-diversity core-metrics-results/faith_pd_vector.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --o-visualization core-metrics-results/faith-pd-group-significance.qzv
+
+qiime diversity alpha-group-significance \
+      --i-alpha-diversity core-metrics-results/evenness_vector.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --o-visualization core-metrics-results/evenness-group-significance.qzv
+
+
+qiime diversity beta-group-significance \
+      --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --m-metadata-column BodySite \
+      --o-visualization core-metrics-results/unweighted-unifrac-body-site-significance.qzv \
+      --p-pairwise
+
+qiime diversity beta-group-significance \
+      --i-distance-matrix core-metrics-results/unweighted_unifrac_distance_matrix.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --m-metadata-column Subject \
+      --o-visualization core-metrics-results/unweighted-unifrac-subject-group-significance.qzv \
+      --p-pairwise
+
+
+qiime emperor plot \
+      --i-pcoa core-metrics-results/unweighted_unifrac_pcoa_results.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --p-custom-axes DaysSinceExperimentStart \
+      --o-visualization core-metrics-results/unweighted-unifrac-emperor-DaysSinceExperimentStart.qzv
+
+
+
+
+qiime emperor plot \
+      --i-pcoa core-metrics-results/bray_curtis_pcoa_results.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --p-custom-axes DaysSinceExperimentStart \
+      --o-visualization core-metrics-results/bray-curtis-emperor-DaysSinceExperimentStart.qzv
+
+
+
+qiime diversity alpha-rarefaction \
+      --i-table table.qza \
+      --i-phylogeny rooted-tree.qza \
+      --p-max-depth 4000 \
+      --m-metadata-file sample-metadata.tsv \
+        --o-visualization alpha-rarefaction.qzv
+
+
+
+
+
+
+qiime feature-classifier classify-sklearn \
+      --i-classifier gg-13-8-99-515-806-nb-classifier.qza \
+      --i-reads rep-seqs.qza \
+      --o-classification taxonomy.qza
+
+qiime metadata tabulate \
+      --m-input-file taxonomy.qza \
+      --o-visualization taxonomy.qzv
+
+
+
+
+qiime taxa barplot \
+      --i-table table.qza \
+      --i-taxonomy taxonomy.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --o-visualization taxa-bar-plots.qzv
+
+
+qiime feature-table filter-samples \
+      --i-table table.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --p-where "BodySite='gut'" \
+      --o-filtered-table gut-table.qza
+
+
+qiime composition add-pseudocount \
+      --i-table gut-table.qza \
+      --o-composition-table comp-gut-table.qza
+
+
+qiime composition ancom \
+      --i-table comp-gut-table.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --m-metadata-column Subject \
+      --o-visualization ancom-Subject.qzv
+
+qiime taxa collapse \
+      --i-table gut-table.qza \
+      --i-taxonomy taxonomy.qza \
+      --p-level 6 \
+      --o-collapsed-table gut-table-l6.qza
+
+qiime composition add-pseudocount \
+      --i-table gut-table-l6.qza \
+      --o-composition-table comp-gut-table-l6.qza
+
+qiime composition ancom \
+      --i-table comp-gut-table-l6.qza \
+      --m-metadata-file sample-metadata.tsv \
+      --m-metadata-column Subject \
+      --o-visualization l6-ancom-Subject.qzv
