0ae410e33426f2c605b5029c150434b07ff897d5
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-05-29 17:44:00 +0200

    generate  krona plot from  qiime tsv fike

---
+++ bin/qiime2krona.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import itertools
+import csv
+
+
+
+
+class Qiime2Krona(object):
+
+    def __init__(self):
+        
+        self.qiime_taxa_fp =  open("level-15.csv", newline='')
+        self.filter_func = lambda x : x == 'BarcodeSequence'
+
+
+        
+    def extract_tables(self):
+        
+         header  = next(self.qiime_taxa_fp).split(",")
+         csv_reader = csv.DictReader(self.qiime_taxa_fp,header)
+         for row in csv_reader:
+             sample_id  = row['index']
+             fname = '_Krona.'.join([sample_id,'tsv'])
+             file_obj = open(fname, "w")
+             filtered_row = self.take_until(row, self.filter_func)
+             for taxon,frequency in filtered_row.items():
+                 if taxon.startswith("D"):
+                     taxon ='\t'.join([ taxon.strip().split('__')[1:][0] for taxon in taxon.split(";") ]).strip()
+                     print("{}\t{}".format(frequency, taxon), file=file_obj)
+             
+             
+    def take_until(self, row, predicate):
+        filtered_row = {}
+        for key, value in row.items():
+            if predicate(key):
+                return filtered_row 
+            filtered_row[key] = value
+
+        return filtered_row
+    
+        
+
+if __name__ == "__name__":
+    qiime2krona =  Qiime2Krona()
+    qiime2krona.extract_tables()
