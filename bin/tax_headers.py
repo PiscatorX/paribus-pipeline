de7d2a8c99637858fb1b460aec8c702ea514441d
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-08-10 18:38:56 +0200

    prepare taxonomy table for phyloseq

--- bin/tax_headers.py
+++ bin/tax_headers.py
@@ -14,12 +14,13 @@ class  GenPhyloSeq(object):
         parser = argparse.ArgumentParser(description="""generate phyloseq table from taxonomy and feture-table""")
         parser.add_argument("taxonomy", help="taxonomy tsv file")
         parser.add_argument("feature_table",help ="feature table tsv file")
-        parser.add_argument("-o", "--output", help ="output file", default="phyloseq_table.csv")
-        
+        parser.add_argument("-o", "--output", help ="output file", default="phyloseq_table.tsv")
+        parser.add_argument("-t", "--taxonomy_out", help ="output file for reformated taxonomy", default="phyloseq_Tax.tsv")
         args, unknown = parser.parse_known_args()
         self.taxa_table_fp  = open(args.taxonomy)
         self.feature_table_fp  = open(args.feature_table)
         self.output_fp = open(args.output, "w")
+        self.taxa_out_fp = open(args.taxonomy_out, "w")
         
     def build_taxa_table(self):
        
@@ -28,20 +29,20 @@ class  GenPhyloSeq(object):
        series_data = [] 
        for row in tsv_reader:
            name  = row['#OTUID']
-           data  = [name] + [ taxon.split('__')[1] for taxon in  row['Taxon'].split(";") if taxon.strip() ]
+           data  = [name] + [ taxon.split('__')[1] for taxon in  row['Taxon'].strip().split(";") if taxon.strip() ]
            series_data.append(pd.Series(data, name = name))
                                                        
        df_taxon_combined = pd.concat(series_data, axis=1)
        self.df_taxon_table= df_taxon_combined.T
        table_cols = self.df_taxon_table.columns
-       colnames = ["#OTUID"] + [ '_'.join(['Level', str(i) ]) for i in  range(1, len(table_cols)+1) ]
+       colnames = ["OTUID"] + [ '_'.join(['Rank', str(i) ]) for i in  range(1, len(table_cols)+1) ]
        self.df_taxon_table.rename(columns = dict(zip(table_cols, colnames )),  inplace=True)
-        
+       self.df_taxon_table.to_csv(self.taxa_out_fp, sep="\t", index=False)
        
     def merge_df(self):
         self.df_feture_table = pd.read_csv(self.feature_table_fp, header=1, delimiter="\t")
-        self.df_feture_table.rename(columns = {"#OTU ID": "#OTUID"},  inplace = True)
-        self.tax_feature_merged = pd.merge(self.df_feture_table, self.df_taxon_table, on="#OTUID")
+        self.df_feture_table.rename(columns = {"#OTU ID": "OTUID"},  inplace = True)
+        self.tax_feature_merged = pd.merge(self.df_feture_table, self.df_taxon_table, on="OTUID")
         self.tax_feature_merged.to_csv(self.output_fp,sep="\t", index=False)
         print(self.tax_feature_merged.head(10))
         
